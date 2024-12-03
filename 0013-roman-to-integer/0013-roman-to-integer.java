8c660f1d0984070da716beb305238b82c4f30de6
CdrePuddles <101607421+CdrePuddles@users.noreply.github.com>
2024-12-03 11:45:43 -0800

    Time: 7 ms (12.36%), Space: 44.7 MB (59.15%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        int number = 0;
+        
+        Map<Character, Integer> numerals = new TreeMap<>();
+        numerals.put('I', 1);
+        numerals.put('V', 5);
+        numerals.put('X', 10);
+        numerals.put('L', 50);
+        numerals.put('C', 100);
+        numerals.put('D', 500);
+        numerals.put('M', 1000);
+
+        for (int i = 0; i < s.length(); i++) {
+            if(i>0 && numerals.get(s.charAt(i-1)) < numerals.get(s.charAt(i))) {
+                number = number - numerals.get(s.charAt(i-1))*2 + numerals.get(s.charAt(i));
+            } else {
+                number += numerals.get(s.charAt(i));
+            }
+        }
+        return number;
+    }
+}
