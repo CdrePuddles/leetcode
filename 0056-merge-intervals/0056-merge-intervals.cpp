2a94a088d50dbe204c759da4e6ca15076fa78d4a
Kyle Jones <101607421+CdrePuddles@users.noreply.github.com>
2025-03-07 23:34:40 -0800

    Time: 61 ms (8.16%), Space: 23.8 MB (59.47%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,32 @@
+#include <unordered_map>
+class Solution {
+public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+
+        return a[0] < b[0];
+    }
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ret;
+        sort(intervals.begin(), intervals.end(), cmp);
+        cout << "lmao" << endl;
+
+        vector<int> temp = intervals[0];
+
+
+        for (int i = 1; i < intervals.size(); i++) {
+            cout << i << endl;
+            if (intervals[i][0] <= temp[1] && intervals[i][0] >= temp[0]) {
+                temp[1] = max(temp[1], intervals[i][1]);
+            } else {
+                ret.push_back(temp);
+                temp = intervals[i];
+            }
+        }
+
+         ret.push_back(temp);
+
+
+        return ret;
+    }
+};
