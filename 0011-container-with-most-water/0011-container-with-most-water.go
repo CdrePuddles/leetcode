30ce5237a332497e6bbc9d065834e90898630ccf
Kyle Jones <101607421+CdrePuddles@users.noreply.github.com>
2025-05-08 16:21:28 -0700

    Time: 3 ms (14.75%), Space: 9.5 MB (69.33%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -0,0 +1,18 @@
+func maxArea(height []int) int {
+    ret := 0
+    size := len(height)
+    r := size-1
+
+    for l := 0; l < size; l++ {
+
+        for ; l < r && height[l] >= height[r]; {
+            ret = max(ret, min(height[l], height[r]) * (r-l))
+            r--
+        }
+                    ret = max(ret, min(height[l], height[r]) * (r-l))
+
+    }
+
+    return ret
+
+}
