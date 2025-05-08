41232ae4dd2e2ae04a5775c7ea3165450874cda4
Kyle Jones <101607421+CdrePuddles@users.noreply.github.com>
2025-05-07 20:02:35 -0700

    Time: 47 ms (39.51%), Space: 90.7 MB (25.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,17 @@
-#include <unordered_map>
+#include<unordered_map>
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> dict;
 
-        for (int num : nums) {
-            if (dict.find(num) == dict.end()) {
-                dict[num] = 1;
-            } else {
+         for (int num : nums) {
+            if (dict.find(num) != dict.end()) {
                 return true;
             }
-        }
+            dict[num]++;
+         }
+
         return false;
     }
 };
