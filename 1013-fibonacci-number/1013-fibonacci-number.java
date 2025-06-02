5fa203ddb3c513a4137d215870637f815031f484
Kyle Jones <101607421+CdrePuddles@users.noreply.github.com>
2025-06-02 14:05:49 -0700

    Time: 0 ms (100%), Space: 41 MB (6.97%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int old = 0;
+        int newy = 1;
+        int hold = old;
+        int ans = 0;
+
+
+        for (int count = 1; count < n; count++) {
+            //ans = old + newy;
+            hold = old + newy; 
+            old = newy;
+            newy = hold;
+        }
+
+        return newy;
+    }
+}
