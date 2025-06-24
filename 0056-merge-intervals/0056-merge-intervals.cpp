de3a836b59ce42c86d98ccebae1cb0e4280c2042
Kyle Jones <101607421+CdrePuddles@users.noreply.github.com>
2025-06-24 13:18:12 -0700

    Time: 4 ms (69.19%), Space: 23.8 MB (57.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,31 +1,28 @@
-#include <unordered_map>
 class Solution {
-public:
+private: 
     static bool cmp(vector<int> &a, vector<int> &b) {
-
-        return a[0] < b[0];
+            return a[0] < b[0];
     }
-
+public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ret;
         sort(intervals.begin(), intervals.end(), cmp);
-        vector<int> temp = intervals[0];
-        vector<int> innerTemp;
+        int first = intervals[0][0];
+        int last = intervals[0][1];
 
+        for (int i = 1; i < intervals.size() ; i++) {
 
-        for (int i = 1; i < intervals.size(); i++) {
-            innerTemp = intervals[i];
-            if (innerTemp[0] <= temp[1] && innerTemp[0] >= temp[0]) {
-                temp[1] = max(temp[1], innerTemp[1]);
+            if (intervals[i][0] <= last && intervals[i][1] >= first) {
+                first = min(first, intervals[i][0]);
+                last = max(last, intervals[i][1]);
             } else {
-                ret.push_back(temp);
-                temp = innerTemp;
+                ret.push_back({first, last});
+                first = intervals[i][0];
+                last = intervals[i][1];
             }
         }
 
-         ret.push_back(temp);
-
-
+        ret.push_back({first, last});
         return ret;
     }
 };
